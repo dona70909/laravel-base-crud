2d276a4151bb59b1e6fcbdc1333c8ae2457a2f44
Donatella7 <campaniellodonatella@gmail.com>
2022-05-17 17:28:17 +0200

    Check array comics It works

--- resources/views/partials/main/main_cards.blade.php
+++ resources/views/partials/main/main_cards.blade.php
@@ -1,7 +1,15 @@
 <section class="container-fluid">
     <div class="row">
-        <div class="col-12">
-            cards index
-        </div>
+        @foreach ($comics as $comic)
+            <div class="card col-3" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @endforeach
+        
     </div>
 </section>
