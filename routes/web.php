a7787539926aa8ac27002f7d91acbea5493925f4
Donatella7 <campaniellodonatella@gmail.com>
2022-05-17 15:42:49 +0200

    Done Model -> Controller-> Migration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guests.comic.index');
-});
+Route::get('/', [ComicController::class,'index'] ) ;
